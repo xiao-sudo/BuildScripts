7e9f6970fe871004cd2231d487abd38b1e2c6f35
Xiao gang <85089925@qq.com>
2022-08-15 17:52:32 +0800

    1. add SINT, FIXED 2. add field_index to Field 3. export csv by tag, filtered by tag_filter

--- Table/common/table.py
+++ Table/common/table.py
@@ -48,32 +48,6 @@ def is_server_only_tag(tag: Tag):
     return Tag.Server == tag
 
 
-class ElemType(Enum):
-    Int = 0
-    Float = 1
-    Str = 2
-    Bool = 3
-    Unknown = 4
-
-    @staticmethod
-    def from_str(type_str: str):
-        elem_type_str = type_str[:3]
-
-        if 'INT' == elem_type_str:
-            return ElemType.Int
-
-        if 'FLO' == elem_type_str:
-            return ElemType.Float
-
-        if 'STR' == elem_type_str:
-            return ElemType.Str
-
-        if 'BOO' == elem_type_str:
-            return ElemType.Bool
-
-        return ElemType.Unknown
-
-
 class ObjectType(Enum):
     Primitive = 0
     Array = 1
@@ -90,6 +64,50 @@ class ObjectType(Enum):
         return ObjectType.Array
 
 
+class ElemType(Enum):
+    Int = 0
+    SInt = 1
+    Fixed = 2
+    Float = 3
+    Str = 4
+    Bool = 5
+    Unknown = 6
+
+    @staticmethod
+    def from_str(type_str: str, object_type: ObjectType):
+        elem_type_str = ElemType._strip_array(type_str, object_type)
+
+        if 'INT' == elem_type_str:
+            return ElemType.Int
+
+        if 'SINT' == elem_type_str:
+            return ElemType.SInt
+
+        if 'FIXED' == elem_type_str:
+            return ElemType.Fixed
+
+        if 'FLOAT' == elem_type_str:
+            return ElemType.Float
+
+        if 'STRING' == elem_type_str:
+            return ElemType.Str
+
+        if 'BOOL' == elem_type_str:
+            return ElemType.Bool
+
+        return ElemType.Unknown
+
+    @staticmethod
+    def _strip_array(type_str, object_type: ObjectType):
+        if ObjectType.Primitive == object_type:
+            return type_str
+        else:
+            if ObjectType.Array == object_type:
+                return type_str[:-2]
+            else:
+                return type_str[:-4]
+
+
 class FieldType:
     def __init__(self, field_type_str: str):
         self.field_type_str = field_type_str.strip()
@@ -97,30 +115,22 @@ class FieldType:
 
     def _parse(self):
         self.object_type = ObjectType.from_str(self.field_type_str)
-        self.elem_type = ElemType.from_str(self.field_type_str)
+        self.elem_type = ElemType.from_str(self.field_type_str, self.object_type)
 
     def __repr__(self):
         return 'FieldType(%r)' % self.field_type_str
 
 
 class Field:
-    def __init__(self, name, index=None, tag=None):
+    def __init__(self, name, col=None, tag=None):
         self.field_type = None
-        self.index = index
+        self.col = col
+        self.field_index = 0
         self.tag = tag
         self.name = name
 
-    def set_index(self, index: int):
-        self.index = index
-
-    def set_field_type(self, field_type: FieldType):
-        self.field_type = field_type
-
-    def set_field_tag(self, tag: Tag):
-        self.tag = tag
-
     def __repr__(self):
-        return 'Field(%r, %r, %r)' % (self.name, self.index, self.tag)
+        return 'Field(%r, %r, %r)' % (self.name, self.col, self.tag)
 
     def to_value(self, v):
         if ObjectType.Primitive == self.field_type.object_type:
@@ -155,17 +165,33 @@ class Header:
     def get_fields(self):
         return self._fields
 
-    def get_csv(self):
-        tags = []
-        types = []
-        names = []
+    # def get_csv(self):
+    #     tags = []
+    #     types = []
+    #     names = []
+    #
+    #     for field in self._fields:
+    #         tags.append(field.tag.to_str())
+    #         types.append(field.field_type.field_type_str)
+    #         names.append(field.name)
+    #
+    #     return [tags, types, names]
+
+    def extract_csv(self, tag_filter):
+        """
+        extract csv fields with filter
+        :param tag_filter: filter tag, client or sever
+        :return: [rs, fields], has filtered info when rs = True
+        """
+        rs = False
+        fields = []
 
         for field in self._fields:
-            tags.append(field.tag.to_str())
-            types.append(field.field_type.field_type_str)
-            names.append(field.name)
+            if tag_filter(field.tag):
+                rs = True
+                fields.append(field)
 
-        return [tags, types, names]
+        return [rs, fields]
 
     def __repr__(self):
         return 'Header(%r)' % self.name
@@ -184,13 +210,52 @@ class Table:
     def add_row(self, row: list):
         self.rows.append(row)
 
-    def export_csv(self, csv_dir: str):
+    def export_csv(self, csv_dir: str, tag_filter=lambda tag: True):
         target_csv_path = f"{csv_dir}/{self.name}.csv"
-        with open(target_csv_path, 'wt', encoding='utf-8-sig') as csv_file:
-            writer = csv.writer(csv_file, delimiter=',', lineterminator='\n', quoting=csv.QUOTE_MINIMAL)
-            writer.writerow([self.xls, self.name])
-            self.__export_header(writer)
-            writer.writerows(self.rows)
 
-    def __export_header(self, writer):
-        writer.writerows(self.header.get_csv())
+        rs, filtered_fields = self.header.extract_csv(tag_filter)
+
+        if not rs:
+            return False
+        else:
+            # encoding is utf-8-sig (utf8-bom)
+            # excel determine the file is utf8 encoding through bom,
+            # without bom excel assumes the csv is encoded by current Windows codepage
+            with open(target_csv_path, 'wt', encoding='utf-8-sig') as csv_file:
+                writer = csv.writer(csv_file, delimiter=',', lineterminator='\n', quoting=csv.QUOTE_MINIMAL)
+                writer.writerow([self.xls, self.name])
+                writer.writerows(Table._header_csv(filtered_fields))
+                writer.writerows(self._content_csv(filtered_fields))
+
+            return True
+
+    @staticmethod
+    def _header_csv(filtered_fields: list):
+        tags = []
+        types = []
+        names = []
+
+        for field in filtered_fields:
+            tags.append(field.tag.to_str())
+            types.append(field.field_type.field_type_str)
+            names.append(field.name)
+
+        return [tags, types, names]
+
+    def _content_csv(self, filtered_fields: list):
+        field_indices = []
+        for field in filtered_fields:
+            field_indices.append(field.field_index)
+
+        filtered_rows = []
+        for row in self.rows:
+            filtered_row = []
+            for field_index in field_indices:
+                if field_index < len(row):
+                    filtered_row.append(row[field_index])
+                else:
+                    print(f'{field_index} in {self.name} out of range')
+
+            filtered_rows.append(filtered_row)
+
+        return filtered_rows
