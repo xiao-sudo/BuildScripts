e6d7d3b0bbb7a8374aa7780890fc58e075738e54
Xiao gang <85089925@qq.com>
2022-08-22 18:11:14 +0800

    1. remove check_text

--- Table/common/field.py
+++ Table/common/field.py
@@ -1,15 +1,14 @@
 from .data import DataType
-from .elem import ElemAnalyzer
 
 
 class Field:
-    def __init__(self, tag, field_name: str, sheet_col: int, field_index: int, data_type: DataType, primary=False):
+    def __init__(self, tag, field_name: str, sheet_col: int, data_type: DataType, primary=False):
         self.tag = tag
         self.field_name = field_name
         self.sheet_col = sheet_col
-        self.field_index = field_index
         self.data_type = data_type
         self.primary = primary
+        self.field_index = -1
 
     def __str__(self):
         return f'{self.field_name} tag: {self.tag}, sheet_col:{self.sheet_col}, ' \
@@ -17,7 +16,3 @@ class Field:
 
     def update_field_index(self, new_index: int):
         self.field_index = new_index
-
-    def check_text(self, text: str):
-        checker = ElemAnalyzer.get_checker(self.data_type.elem_type, self.data_type.organization)
-        return checker(text)
