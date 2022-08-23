2b30cbf563b95778936c63fb24ea0d9dcee6fba5
Xiao gang <85089925@qq.com>
2022-08-23 17:31:54 +0800

    1. add single_xls_parser and single_sheet_arg_parser

--- Table/common/arg.py
+++ Table/common/arg.py
@@ -14,3 +14,25 @@ def new_arg_parser():
 def prepare_csharp_parser(parser):
     parser.add_argument("--cs_pb", required=True, help="out csharp protobuf scripts dir")
     parser.add_argument("--cs_bytes", required=True, help="out csharp protobuf data dir")
+
+
+def single_xls_arg_parser():
+    arg_parser = argparse.ArgumentParser(description='Single Xls Parser')
+    _preparse_single_xls_parser(arg_parser)
+
+    return arg_parser
+
+
+def single_sheet_arg_parser():
+    arg_parser = argparse.ArgumentParser(description='Single Sheet Parser')
+    _preparse_single_xls_parser(arg_parser)
+    arg_parser.add_argument('--sheet_name', required=True, help='sheet name')
+
+    return arg_parser
+
+
+def _preparse_single_xls_parser(parser):
+    parser.add_argument('--xls_path', required=True, help='xls file path')
+    parser.add_argument('--csv_dir', required=True, help='export csv dir')
+    parser.add_argument('--pb_dir', required=True, help='export pb dir')
+    return parser
