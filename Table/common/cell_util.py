d74c0ef54207bcfbe2c16f6abc43f3ff87635ca1
Xiao gang <85089925@qq.com>
2022-08-24 15:31:37 +0800

    1. add cell_xls_col, convert col index to A-Z (same as xlsx)

--- Table/common/cell_util.py
+++ Table/common/cell_util.py
@@ -13,6 +13,18 @@ def cell_xls_coord_str(row, col):
     return cell coordinate str by zero based row and col
     :param row: zero based row number
     :param col: zero based col number
-    :return: cell coordinate str
+    :return: cell coordinate str "(number from 1 to n, A-Z)"
     """
-    return f'({row + 1},{col + 1})'
+    return f'({row + 1},{xls_column_lookup(col)})'
+
+
+def cell_xls_col(col):
+    return xls_column_lookup(col)
+
+
+def cell_xls_row(row):
+    return row + 1
+
+
+def xls_column_lookup(col):
+    return chr(col + ord('A'))
