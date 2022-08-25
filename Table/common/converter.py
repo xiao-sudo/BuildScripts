314f05e3d00f0a362f6b17d5fb8a743e387c8a73
Xiao gang <85089925@qq.com>
2022-08-25 20:20:38 +0800

    1. add TypeToCSVStr, TypeToProtoStr

--- Table/common/converter.py
+++ Table/common/converter.py
@@ -1,36 +1,8 @@
-class TextConverter:
-    def to_client_csv_str(self) -> str:
-        """
-        convert to client csv str
-        :return: str
-        """
+class TypeToCSVStr:
+    def to_csv_str(self) -> str:
         pass
 
-    def to_server_csv_str(self) -> str:
-        """
-        convert to server csv str
-        :return:
-        """
-        pass
 
+class TypeToProtoStr:
     def to_proto_str(self) -> str:
-        """
-        convert to protocol buffer str
-        :return:
-        """
-        pass
-
-
-class DefaultValueConverter:
-    def default_value(self):
-        pass
-
-
-class TextToValueConverter:
-    def parse_text_to_value(self, text: str):
-        """
-        parse text to value
-        :param text: input text
-        :return: [bool, value_or_err]
-        """
         pass
