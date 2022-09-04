61354de9824cfb5f45b42ec81b91b51205da020b
Xiao gang <85089925@qq.com>
2022-09-04 17:56:46 +0800

    1. add values to row

--- Table/common/row.py
+++ Table/common/row.py
@@ -12,6 +12,10 @@ class Row:
     def __init__(self):
         self.semantic = RowSemantic.Content
         self.content = []
+        self.values = []
 
-    def add_value(self, value: str):
+    def add_csv(self, value: str):
         self.content.append(value)
+
+    def add_value(self, value):
+        self.values.append(value)
